b498e91eacaa9758bfdd4c27bce6921694a45cbb
Luccas Avila <luccasavila62@gmail.com>
2020-03-09 14:02:22 -0300

    styled-components and Global Styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <h1>Word Frequency Counter</h1>;
+  return (
+    <>
+      <h1>Word Frequency Counter</h1>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;

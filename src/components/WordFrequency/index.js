eb9de5fd8686d1cd2bb83d2316865bb98d5fceae
Luccas Avila <luccasavila62@gmail.com>
2020-03-11 12:47:35 -0300

    wordFrequency working

--- src/components/WordFrequency/index.js
+++ src/components/WordFrequency/index.js
@@ -1,16 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Container, Title, List, Item } from './styles';
 
 import { TextContext } from '../../context/TextContext';
 
+let timer;
+
 const WordFrequency = () => {
-  const context = useContext(TextContext);
+  const { wordFrequency, text } = useContext(TextContext);
+  const [wordInfo, setWordInfo] = useState([]);
+
+  useEffect(() => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    timer = setTimeout(() => {
+      setWordInfo(wordFrequency);
+    }, 1000);
+  }, [text]);
 
   return (
     <Container>
       <Title>Word Frequency:</Title>
       <List>
-        <Item>1 - Lorem (15%)</Item>
+        {wordInfo.map(word => (
+          <Item key={word[0]}>{`${word[1]} - ${word[0]} (${word[2]}%)`}</Item>
+        ))}
       </List>
     </Container>
   );
